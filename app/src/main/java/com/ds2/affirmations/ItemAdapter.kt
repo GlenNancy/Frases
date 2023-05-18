ef8fa455045cd2b8576897e38b8f5001df354e2d
glennancy <glennancy24@gmail.com>
2023-05-17 21:13:16 -0300

    Adicionando Frases e Imagens

--- app/src/main/java/com/ds2/affirmations/ItemAdapter.kt
+++ app/src/main/java/com/ds2/affirmations/ItemAdapter.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.ds2.affirmations.model.Affirmation
@@ -13,6 +14,7 @@ List<Affirmation>) : RecyclerView.Adapter<ItemAdapter.ItemViewHolder>() {
     class ItemViewHolder(private val view: View) :
     RecyclerView.ViewHolder(view) {
         val textView: TextView = view.findViewById(R.id.item_title)
+        val imageView: ImageView = view.findViewById(R.id.item_image)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int):
@@ -29,6 +31,7 @@ List<Affirmation>) : RecyclerView.Adapter<ItemAdapter.ItemViewHolder>() {
     override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
         val item = dataset[position]
         holder.textView.text = context.resources.getString(item.stringResourceId)
+        holder.imageView.setImageResource(item.imageResourceId)
     }
 }
 
--- app/src/main/java/com/ds2/affirmations/data/DataSource.kt
+++ app/src/main/java/com/ds2/affirmations/data/DataSource.kt
@@ -6,16 +6,16 @@ import com.ds2.affirmations.model.Affirmation
 class Datasource {
     fun loadAffirmations(): List<Affirmation> {
         return listOf<Affirmation>(
-            Affirmation(R.string.affirmation1),
-            Affirmation(R.string.affirmation2),
-            Affirmation(R.string.affirmation3),
-            Affirmation(R.string.affirmation4),
-            Affirmation(R.string.affirmation5),
-            Affirmation(R.string.affirmation6),
-            Affirmation(R.string.affirmation7),
-            Affirmation(R.string.affirmation8),
-            Affirmation(R.string.affirmation9),
-            Affirmation(R.string.affirmation10)
+            Affirmation(R.string.affirmation1, R.drawable.imagem1),
+            Affirmation(R.string.affirmation2, R.drawable.imagem2),
+            Affirmation(R.string.affirmation3, R.drawable.imagem3),
+            Affirmation(R.string.affirmation4, R.drawable.imagem4),
+            Affirmation(R.string.affirmation5, R.drawable.imagem5),
+            Affirmation(R.string.affirmation6, R.drawable.imagem6),
+            Affirmation(R.string.affirmation7, R.drawable.imagem7),
+            Affirmation(R.string.affirmation8, R.drawable.imagem8),
+            Affirmation(R.string.affirmation9, R.drawable.imagem9),
+            Affirmation(R.string.affirmation10, R.drawable.imagem10)
         )
     }
 }
